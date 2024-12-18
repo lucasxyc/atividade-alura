eba598cd9191d690516020ab4b62185afc75364d
Lucas Nogueira <lucasnogueirae2@gmail.com>
2024-12-18 13:02:02 -0300

    Criação do pacote "UI" contendo a classe "Menu".

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,17 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.services.ConsumoAPI;
 import br.com.alura.screenmatch.services.ConverterDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -23,6 +29,21 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 
 		System.out.println(dados);
+
+		json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=b5f82fb8&t=supernatural&season=1&episode=2");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+
+		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int numeroTemporada = 1; numeroTemporada<dados.totalTemporadas(); numeroTemporada++) {
+			json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=b5f82fb8&t=supernatural&season="+ numeroTemporada +"&");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+
+		}
+		temporadas.forEach(System.out::println);
 
 	}
 }
