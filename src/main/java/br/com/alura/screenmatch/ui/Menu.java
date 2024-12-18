b84a2d8a9bd025c847621de0bcda5e8dd8678634
Lucas Nogueira <lucasnogueirae2@gmail.com>
2024-12-18 13:01:17 -0300

    Criação do pacote UI

---
+++ src/main/java/br/com/alura/screenmatch/ui/Menu.java
@@ -0,0 +1,15 @@
+package br.com.alura.screenmatch.ui;
+
+import java.util.Scanner;
+
+public class Menu {
+    public void exibirMenuPrincipal() {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Pesquisar série: ");
+        var pesquisaDoUsuario = leitura.nextLine();
+
+
+
+    }
+}
